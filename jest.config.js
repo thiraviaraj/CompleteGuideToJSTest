887fde1566e91505f94c0eb4161c582f700b8408
thiraviaraj <thiraviaraj1990@gmail.com>
2024-01-21 15:55:49 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ const config = {
         statements: -10,
       },
     },
+  coverageReporters: ['lcov', 'text', 'html'], // Add this line for HTML coverage report
+  collectCoverage: true, // Ensure coverage is collected
+  coverageDirectory: 'coverage', // Set the directory for coverage reports
   };
   
   module.exports = config;
